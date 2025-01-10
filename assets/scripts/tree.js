a6f15ea55e864def943863a725ed821d0aeb89cc
astrobyte21 <167555091+astrobyte21@users.noreply.github.com>
2025-01-10 16:05:01 +0545

    Update tree.js
    dhkagd

--- assets/scripts/tree.js
+++ assets/scripts/tree.js
@@ -1,13 +1,13 @@
 const tree = [
   {
     name: "Cycus",
-    pic: "/assets/images/trees/1.jpg",
+    pic: "/assets/images/trees/1.jiff",
     price1: "Rs 500",
     price2: "Rs 10000",
   },
     {
     name: "Orange tree",
-    pic: "/assets/images/trees/2.jpg",
+    pic: "/assets/images/trees/2.jiff",
     price1: "Rs 250",
     price2: "Rs 2500",
   },
