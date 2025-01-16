2eec422644f79d160f083331e9f0cba748a6e50e
astrobyte21 <167555091+astrobyte21@users.noreply.github.com>
2025-01-16 18:48:13 +0545

    Update flower.js
    added all the flowers in new format

--- assets/scripts/flower.js
+++ assets/scripts/flower.js
@@ -96,224 +96,224 @@ const flowers = [
     price1: "Rs 1850",
     price2: "",
   },
-  /*
+  
   {
     name: "Giraniam",
-    pic: "assets/images/flowers/1.jfif",
+    pic: "assets/images/flowers/1.jpg",
     price1: "RS 250",
     price2: "RS 850",
   },
     {
     name: "Lalupate",
-    pic: "assets/images/flowers/4.jfif",
+    pic: "assets/images/flowers/4.jpg",
     price1: "RS 250",
     price2: "RS 850",
   },
   {
     name: "Godawori",
-    pic: "assets/images/flowers/5.jfif",
+    pic: "assets/images/flowers/5.jpg",
     price1: "RS 75",
     price2: "RS 350",
   },
   {
     name: "Christmas Cactus",
-    pic: "assets/images/flowers/6.jfif",
+    pic: "assets/images/flowers/6.jpg",
     price1: "RS 450",
   },
   {
     name: "Vervina",
-    pic: "assets/images/flowers/7.jfif",
+    pic: "assets/images/flowers/7.jpg",
     price1: "RS 75",
   },
   {
     name: "Gloxinia",
-    pic: "assets/images/flowers/8.jfif",
+    pic: "assets/images/flowers/8.jpg",
     price1: "RS 550",
   },
   {
     name: "Adenium",
-    pic: "assets/images/flowers/13.jfif",
+    pic: "assets/images/flowers/13.jpg",
     price1: "RS 1250",
   },
   {
     name: "Dieffenbacia ",
-    pic: "assets/images/flowers/16.jfif",
+    pic: "assets/images/flowers/16.jpg",
     price1: "RS 1050",
   },
   {
     name: "Philodendron",
-    pic: "assets/images/flowers/15.jfif",
+    pic: "assets/images/flowers/15.jpg",
     price1: "RS 1250",
   },
   {
     name: "Lotus",
-    pic: "assets/images/flowers/17.jfif",
+    pic: "assets/images/flowers/17.jpg",
     price1: "RS 750",
   },
   {
     name: "Aglonema",
-    pic: "assets/images/flowers/18.jfif",
+    pic: "assets/images/flowers/18.jpg",
     price1: "RS 1050",
   },
   {
     name: "Croton",
-    pic: "assets/images/flowers/20.jfif",
+    pic: "assets/images/flowers/20.jpg",
     price1: "RS 850",
   },
   {
     name: "Silver dracaena",
-    pic: "assets/images/flowers/21.jfif",
+    pic: "assets/images/flowers/21.jpg",
     price1: "RS 1050",
   },
    {
     name: "Peace Lily",
-    pic: "assets/images/flowers/22.jfif",
+    pic: "assets/images/flowers/22.jpg",
     price1: "RS 1250",
   },
    {
     name: "  Philodendron",
-    pic: "assets/images/flowers/23.jfif",
+    pic: "assets/images/flowers/23.jpg",
     price1: "RS 1250",
   },
    {
     name: "  Chamaedora",
-    pic: "assets/images/flowers/25.jfif",
+    pic: "assets/images/flowers/25.jpg",
     price1: "RS 1250",
   },
    {
     name: " Snake Plant",
-    pic: "assets/images/flowers/26.jfif",
+    pic: "assets/images/flowers/26.jpg",
     price1: "RS 650",
   },
    {
     name: "Table Palm",
-    pic: "assets/images/flowers/27.jfif",
+    pic: "assets/images/flowers/27.jpg",
     price1: "RS 1500",
   },
    {
     name: "Begambeli",
-    pic: "assets/images/flowers/28.jfif",
+    pic: "assets/images/flowers/28.jpg",
     price1: "RS 1050",
   },
    {
     name: " Marigold",
-    pic: "assets/images/flowers/31.jfif",
+    pic: "assets/images/flowers/31.jpg",
     price1: "RS 40",
     price2: "RS 250",
   },
    {
     name: "Golden dhupi",
-    pic: "assets/images/flowers/32.jfif",
+    pic: "assets/images/flowers/32.jpg",
     price1: "RS 250",
     price2: "RS 2500",
   },
    {
     name: " Areca Palm",
-    pic: "assets/images/flowers/33.jfif",
+    pic: "assets/images/flowers/33.jpg",
     price1: "RS 450",
   },
    {
     name: "Yucca",
-    pic: "assets/images/flowers/36.jfif",
+    pic: "assets/images/flowers/36.jpg",
     price1: "RS 1500",
   },
    {
     name: " Hibiscus",
-    pic: "assets/images/flowers/37.jfif",
+    pic: "assets/images/flowers/37.jpg",
     price1: "RS 550",
   },
    {
     name: "  Philodendron",
-    pic: "assets/images/flowers/38.jfif",
+    pic: "assets/images/flowers/38.jpg",
     price1: "RS 3500",
   },
    {
     name: "  geranium",
-    pic: "assets/images/flowers/39.jfif",
+    pic: "assets/images/flowers/39.jpg",
     price1: "RS 1200",
   },
    {
     name: " Nepali Orchid",
-    pic: "assets/images/flowers/40.jfif",
+    pic: "assets/images/flowers/40.jpg",
     price1: "RS 7000",
   },
    {
     name: " Lily flower",
-    pic: "assets/images/flowers/41.jfif",
+    pic: "assets/images/flowers/41.jpg",
     price1: "May Vary",
   },
    {
     name: " Furcaria",
-    pic: "assets/images/flowers/44.jfif",
+    pic: "assets/images/flowers/44.jpg",
     price1: "RS 2000",
   },
    {
     name: " Money plant",
-    pic: "assets/images/flowers/45.jfif",
+    pic: "assets/images/flowers/45.jpg",
     price1: "RS 1500",
   },
    {
     name: " Orchid",
-    pic: "assets/images/flowers/46.jfif",
+    pic: "assets/images/flowers/46.jpg",
     price1: "RS 2050",
   },
    {
     name: "  pilargonium",
-    pic: "assets/images/flowers/48.jfif",
+    pic: "assets/images/flowers/48.jpg",
     price1: "RS 350",
     price2: "RS 1050',
   },
    {
     name: "  Rose ",
-    pic: "assets/images/flowers/49.jfif",
+    pic: "assets/images/flowers/49.jpg",
     price1: "RS 250",
   },
    {
     name: " Dianthus",
-    pic: "assets/images/flowers/52.jfif",
+    pic: "assets/images/flowers/52.jpg",
     price1: "RS 250",
   },
    {
     name: "  Clevia",
-    pic: "assets/images/flowers/59.jfif",
+    pic: "assets/images/flowers/59.jpg",
     price1: "RS 450",
   },
    {
     name: "  White Geranium",
-    pic: "assets/images/flowers/58.jfif",
+    pic: "assets/images/flowers/58.jpg",
     price1: "RS 250",
   },
    {
     name: "  Caamelia",
-    pic: "assets/images/flowers/60.jfif",
+    pic: "assets/images/flowers/60.jpg",
     price1: "RS 1500",
   },
    {
     name: "  Lily flower",
-    pic: "assets/images/flowers/62.jfif",
+    pic: "assets/images/flowers/62.jpg",
     price1: "RS 550",
   },
    {
     name: "  Cineraria",
-    pic: "assets/images/flowers/71.jfif",
+    pic: "assets/images/flowers/71.jpg",
     price1: "RS 50",
     price2: "RS 250",
   },
    {
     name: "  Rhapis palm",
-    pic: "assets/images/flowers/82.jfif",
+    pic: "assets/images/flowers/82.jpg",
     price1: "RS 3500",
   },
    {
     name: "  Philodendron",
-    pic: "assets/images/flowers/23.jfif",
+    pic: "assets/images/flowers/23.jpg",
     price1: "RS 1250",
   },
   
   
   
   
-  */
+  
   
   
   
